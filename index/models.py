faeec1c0f7a4e6e6b052954384bddfa64c6ac05b
ramazonmurodov <ramazon19m@gmail.com>
2025-12-27 13:58:06 +0500

    first commit

---
+++ index/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+# Create your models here.
+class NewsCategory(models.Model):
+    category_name = models.CharField(max_length=16, verbose_name='название категории')
+    added_date = models.DateTimeField(auto_now_add=True)
+
+
+class News(models.Model):
+    title = models.CharField(max_length=128, verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Основной текст')
+    category = models.ForeignKey(NewsCategory, on_delete=models.CASCADE, verbose_name='категория')
+    added_date = models.DateTimeField(auto_now_add=True)
